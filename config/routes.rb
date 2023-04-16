b1a999f9d987fef0256145c08bae221960e6bf7b
Shivani Shilpi <shivanishilpi2120@gmail.com>
2023-04-16 16:33:59 +0530

    create user and room

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -7,4 +8,10 @@ Rails.application.routes.draw do
   resources :messages
   resources :chat_room
   # get 'messages/chat_room'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+  resources :rooms
+  resources :users
+  root 'rooms#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_134643) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_16_101128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_134643) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.boolean "is_private", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
