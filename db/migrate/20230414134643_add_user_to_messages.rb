26ce4dc4617385b287e132fd63671a66b159e862
shivani <shivani.shilpi@clecotech.com>
2023-04-17 12:28:23 +0530

    Delete message file

--- db/migrate/20230414134643_add_user_to_messages.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToMessages < ActiveRecord::Migration[7.0]
-  def change
-    add_column :messages, :user, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -14,13 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_16_101128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "messages", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "user"
-  end
-
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.boolean "is_private", default: false
